e0ee63287ef933d4f028b4dad51c5f42829c195e
vankaya <van4ka7@gmail.com>
2021-01-09 18:48:22 +0200

    account

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import FindForm
+from .models import Vacancy
+
+def home_view(request):
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    print (city, language)
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs,
+                                                  'form':form})
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from scraping.views import home_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('', home_view),
+    path('home/', home_view),
 ]
